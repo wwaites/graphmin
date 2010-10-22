8684acf93c582c7bdc463688c1d14805dc698c65
William Waites <ww@styx.org>
2010-10-22 12:20:24 +0200

    separate out initial graph and minimised graph, add an example using rdfs:subClassOf that breaks

--- graphmin.py
+++ graphmin.py
@@ -1,9 +1,15 @@
 from optparse import OptionParser
+import logging
 import sys
 
-from rdflib.Graph import Graph
+from rdflib.Graph import Graph, ConjunctiveGraph, ReadOnlyGraphAggregate
+from FuXi.Rete.Network import ReteNetwork
+from FuXi.Rete.Util import generateTokenSet
+from FuXi.Rete.RuleStore import SetupRuleStore
 from FuXi.Horn.HornRules import HornFromN3
 from FuXi.SPARQL.BackwardChainingStore import TopDownSPARQLEntailingStore
+
+log = logging.getLogger("graphmin")
 
 class GraphMinimiser(object):
     opt_parser = OptionParser(usage="""
@@ -17,6 +23,9 @@ Minimise a graph...
                           default=False,
                           action="store_true",
                           help="debug")
+    opt_parser.add_option("-l", "--logfile",
+                          dest="logfile",
+                          help="logfile")
     opt_parser.add_option("-i", "--input",
                           dest="infile",
                           default="-",
@@ -42,6 +51,13 @@ Minimise a graph...
             cfg = eval(fp.read())
             fp.close()
             self.config.update(cfg)
+        logcfg = {
+            "format": "%(asctime)s - %(levelname)s - %(message)s",
+            "level": logging.DEBUG,
+        }
+        if self.opts.logfile:
+            logcfg.filename = self.opts.logfile
+        logging.basicConfig(**logcfg)
 
     def get_informat(self):
         if self.opts.infile == "-":
@@ -59,31 +75,51 @@ Minimise a graph...
             infile = sys.stdin
         else:
             infile = open(self.opts.infile)
-        if self.opts.outfile == "-":
-            outfile = sys.stdout
-        else:
-            outfile = open(self.opts.outfile, "w+")
 
         facts = Graph()
         facts.parse(infile, format=self.get_informat())
         infile.close()
 
-        rules = HornFromN3(self.opts.rules)
+#        rstore, rgraph = SetupRuleStore()
+#        network = ReteNetwork(rstore)
+#        rules = HornFromN3(self.opts.rules)
+#        for rule in rules:
+#            network.buildNetworkFromClause(rule)
+#        network.feedFactsToAdd(generateTokenSet(facts))
+#
+#        facts += network.inferredFacts
+
+        minimised = Graph()
+        minimised.namespace_manageer = facts.namespace_manager
+        minimised += facts
         
         for s,p,o in facts.triples((None, None, None)):
-            facts.remove((s,p,o))
+            minimised.remove((s,p,o))
+            rules = HornFromN3(self.opts.rules)
             topDownStore = TopDownSPARQLEntailingStore(
-                facts.store,
-                facts,
+                minimised.store,
+                minimised,
                 idb=rules,
+                decisionProcedure=0,
                 derivedPredicates=[p])
             target = Graph(topDownStore)
-            q = "ASK WHERE { %s }" % " ".join(x.n3() for x in (s,p,o))
-            result = target.query(q)
-            if not result.askAnswer.pop():
-                facts.add((s,p,o))
 
-        facts.serialize(outfile, format=self.opts.format)
+            q = "ASK WHERE { %s }" % " ".join(x.n3() for x in (s,p,o))
+            log.debug("QUERY %s" % q)
+            result = target.query(q).askAnswer[0]
+            log.debug("RESULT %s" % result)
+            if not result:
+                minimised.add((s,p,o))
+                if self.opts.debug:
+                    log.info("KEEP %s." % " ".join(x.n3() for x in (s,p,o)))
+            elif self.opts.debug:
+                log.info("DROP %s." % " ".join(x.n3() for x in (s,p,o)))
+
+        if self.opts.outfile == "-":
+            outfile = sys.stdout
+        else:
+            outfile = open(self.opts.outfile, "w+")
+        minimised.serialize(outfile, format=self.opts.format)
         outfile.close()
 
 def graphmin():
